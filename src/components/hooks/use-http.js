8f79f563ad5be039bddd71c298e7da962e9235ad
“longnguyen-dev” <ntlong2603@gmail.com>
2023-02-26 22:07:53 +0700

    Comment initialValue store

--- src/components/hooks/use-http.js
+++ src/components/hooks/use-http.js
@@ -1,32 +1,32 @@
 import { useState } from "react";
 
 const useHttp = () => {
-    const initialValue = [
-        {
-            id: 'm1',
-            name: 'Sushi',
-            description: 'Finest fish and veggies',
-            price: 22.99,
-        },
-        {
-            id: 'm2',
-            name: 'Schnitzel',
-            description: 'A german specialty!',
-            price: 16.5,
-        },
-        {
-            id: 'm3',
-            name: 'Barbecue Burger',
-            description: 'American, raw, meaty',
-            price: 12.99,
-        },
-        {
-            id: 'm4',
-            name: 'Green Bowl',
-            description: 'Healthy...and green...',
-            price: 18.99,
-        },
-    ];
+    // const initialValue = [
+    //     {
+    //         id: 'm1',
+    //         name: 'Sushi',
+    //         description: 'Finest fish and veggies',
+    //         price: 22.99,
+    //     },
+    //     {
+    //         id: 'm2',
+    //         name: 'Schnitzel',
+    //         description: 'A german specialty!',
+    //         price: 16.5,
+    //     },
+    //     {
+    //         id: 'm3',
+    //         name: 'Barbecue Burger',
+    //         description: 'American, raw, meaty',
+    //         price: 12.99,
+    //     },
+    //     {
+    //         id: 'm4',
+    //         name: 'Green Bowl',
+    //         description: 'Healthy...and green...',
+    //         price: 18.99,
+    //     },
+    // ];
 
     const [dataFetch, setDataFetch] = useState();
 

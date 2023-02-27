2df616d6b6ab3595e5e94c80d1f58557b2cbb814
“longnguyen-dev” <ntlong2603@gmail.com>
2023-02-27 09:05:40 +0700

    Fix error invalid class of input postalCode and city

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -61,12 +61,12 @@ const Checkout = (props) => {
                 <input type='text' id='street' ref={streetInputRef} />
                 {!formInputValidity.street && <p>Please enter a valid street!</p>}
             </div>
-            <div className={cityControlClasses}>
+            <div className={postalCodeControlClasses}>
                 <label htmlFor='postal'>Postal Code</label>
                 <input type='text' id='postal' ref={postalCodeInputRef} />
                 {!formInputValidity.postalCode && <p>Please enter a valid postal code!</p>}
             </div>
-            <div className={postalCodeControlClasses}>
+            <div className={cityControlClasses}>
                 <label htmlFor='city'>City</label>
                 <input type='text' id='city' ref={cityInputRef} />
                 {!formInputValidity.city && <p>Please enter a valid city!</p>}
